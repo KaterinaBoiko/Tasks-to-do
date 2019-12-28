254283a9a5919a2be2e25c780503c64679d3ec8e
Kateryna Boiko <katerina.boiko16@gmail.com>
2019-12-28 23:55:14 +0200

    welcome page & favicon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
-import { FooterComponent } from './shared/footer/footer.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    WelcomePageComponent
   ],
   imports: [
     BrowserModule,
